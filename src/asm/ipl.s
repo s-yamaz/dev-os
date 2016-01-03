db44d8038abb73d976e73b4677abea528b5a5dc0
s.yamazaki <razgriz01blaze@gmail.com>
2016-01-03 22:51:34 +0900

    hello worldのアセンブラプログラムを書き直し

--- src/asm/ipl.s
+++ src/asm/ipl.s
@@ -1,5 +1,9 @@
+.code16
+.text
+    jump entry
+
     # FAT12のための記述
-    .byte   0xeb, 0x4e, 0x90
+    .byte   0x90
     .ascii  "HELLOIPL"          # ブートセクタの名前
     .word   512                 # 1セクタの大きさ(512にしなければならない)
     .byte   1                   # クラスタの大きさ(1セクタにしなければならない)
@@ -20,27 +24,26 @@
     .org    .+18
 
 # プログラム本体
-    
-    .byte   0xb8, 0x00, 0x00, 0x8e, 0xd0, 0xbc, 0x00, 0x7c
-    .byte   0x8e, 0xd8, 0x8e, 0xc0, 0xbe, 0x74, 0x7c, 0x8a
-    .byte   0x04, 0x83, 0xc6, 0x01, 0x3c, 0x00, 0x74, 0x09
-    .byte   0xb4, 0x0e, 0xbb, 0x0f, 0x00, 0xcd, 0x10, 0xeb
-    .byte   0xee, 0xf4, 0xeb, 0xfd
+entry:
+    movw    $0, %ax                # レジスタの初期化
+    movw    %ax, %ss
+    movw    $0x7c00, %sp
+    movw    %ax, %ds
+    movw    %ax, %es
 
-# メッセージ部
+    movw    $msg, %si
+putloop:
+    movb    (%si), %al
+    addw    $1, %si
+    cmpb    $0, %al
+    je      fin
+    movb    $0x0e, %ah              # 一文字表示BIOSコール
+    movb    $10, %bx                # カラーコード 15: white
+    int     $0x10                   # ビデオBIOS呼び出し
+    jump    putloop
+fin:
+    hlt
+    jump    fin
 
-    .byte   0x0a, 0x0a          # 改行 x 2
-    .ascii  "Hello, World"
-    .byte   0x0a
-    .byte   0
-
-    .org    0x1fe                  # 0x001feまでスキップする
-
-    .byte   0x55, 0xaa
-
-# ブートセクタ以外の部分の記述
-
-    .byte 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-    .org .+4600
-    .byte 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-    .org .+1469432
+.data
+msg: .string "Hello, World!\n"
