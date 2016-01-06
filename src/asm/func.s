958d1bf7ae0d11f177fabc607f9ecccd99ceb282
s.yamazaki <razgriz01blaze@gmail.com>
2016-01-06 22:56:51 +0900

    C言語からメモリへの書込みを実装
    - VRAMにカラーコード15を書き込み画面を真っ白に

--- src/asm/func.s
+++ src/asm/func.s
@@ -1,7 +1,18 @@
 .file "func.s"
-.section .text
 .global io_hlt
+.global write_mem8
+
+.arch i486
+
+.section .text
 
 io_hlt:
     hlt
     ret
+
+write_mem8:                 # void write_mem8(int addr, int data)
+    movl    4(%esp), %ecx   # [%esp + 4]にaddrが入っているのでそれをecxに読み込む
+    movb    8(%esp), %al    # [%esp + 8]にdataが入っているのでそれをALに読む
+    movb    %al, (%ecx)
+    ret
+    
--- src/c/bootpack.c
+++ src/c/bootpack.c
@@ -1,7 +1,12 @@
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void SosMain(void) {
-fin:
-    io_hlt();
-    goto fin;
+    int i;
+    for (i = 0xa0000; i<= 0xaffff; i++) {
+        write_mem8(i, 15);
+    }
+    for (;;) {
+        io_hlt();
+    }
 }
