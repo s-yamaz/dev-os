f1aef29b528c7301bc1ddf75d5cd7b5004a173d5
s.yamazaki <razgriz01blaze@gmail.com>
2016-01-07 14:59:20 +0900

    画面に矩形を表示するように実装した

--- src/asm/func.s
+++ src/asm/func.s
@@ -1,5 +1,9 @@
 .file "func.s"
 .global io_hlt
+.global io_cli
+.global io_in8, io_in16, io_in32
+.global io_out8, io_out16, io_out32
+.global io_load_eflags, io_store_eflags
 .global write_mem8
 
 .arch i486
@@ -8,4 +12,62 @@
 
 io_hlt:
     hlt
+    ret
+
+io_cli:             # void io_cli
+    cli
+    ret
+
+io_sti:
+    sti
+    ret
+
+io_stihlt:
+    sti
+    hlt
+    ret
+
+io_in8:
+    movl    4(%esp), %edx
+    movl    $0, %eax
+    inb     %dx, %al
+    ret
+
+io_out8:
+    movl    4(%esp), %edx   # port
+    movl    8(%esp), %eax   # data
+    outb    %al, %dx
+    ret
+
+io_in16:
+    movl    4(%esp), %edx
+    movl    $0, %eax
+    inw     %dx, %ax
+    ret
+
+io_out16:
+    movl    4(%esp), %edx
+    movl    8(%esp), %eax
+    outw    %ax, %dx
+    ret
+
+io_in32:
+    movl    4(%esp), %edx
+    inl     %dx, %eax
+
+io_out32:
+    movl    4(%esp), %edx
+    movl    8(%esp), %eax
+    outl    %eax, %dx
+    ret
+
+io_load_eflags:
+    pushfl
+    pop     %eax
+    ret
+
+io_store_eflags:
+    movl    4(%esp), %eax
+    push    %eax
+    popfl
     ret
--- src/c/bootpack.c
+++ src/c/bootpack.c
@@ -1,14 +1,93 @@
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
 void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize,
+        unsigned char c, int x0, int y0, int x1, int y1);
 
 void SosMain(void) {
     int i;
     char *p;
 
-    for (i = 0xa0000; i <= 0xaffff; i++) {
-        p = (char*) i; /* 番地を代入 */
-        *p = i & 0x0f;
-    }
+    init_palette(); /* 色パレットを設定 */
+
+    p = (char *) 0xa0000;
+
+    boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+    boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+    boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
+
     for (;;) {
         io_hlt();
     }
+}
+
+void init_palette(void) {
+    unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00,   /* brack */
+        0xff, 0x00, 0x00,   /* red */
+        0x00, 0xff, 0x00,   /* green */
+        0xff, 0xff, 0x00,   /* yellow */
+        0x00, 0x00, 0xff,   /* blue */
+        0xff, 0x00, 0xff,   /* purple */
+        0x00, 0xff, 0xff,   /* sky */
+        0xff, 0xff, 0xff,   /* white */
+        0xc6, 0xc6, 0xc6,   /* gray */
+        0x84, 0x00, 0x00,   /* dark red */
+        0x00, 0x84, 0x00,   /* dark green */
+        0x84, 0x84, 0x00,   /* dark yellow */
+        0x00, 0x00, 0x84,   /* dark blue */
+        0x84, 0x00, 0x84,   /* dark purple */
+        0x00, 0x84, 0x84,   /* dark sky */
+        0x84, 0x84, 0x84,   /* dark gray */
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb) {
+    int i, eflags;
+    eflags = io_load_eflags(); /*割り込み許可フラグの値を記録する*/
+    io_cli(); /* 許可フラグを0にして割り込み禁止にする */
+    io_out8(0x03c8, start);
+    for(i = start; i <= end ; i++) {
+        io_out8(0x3c9, rgb[0] / 4);
+        io_out8(0x3c9, rgb[1] / 4);
+        io_out8(0x3c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);
+    return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c,
+        int x0, int y0, int x1, int y1) {
+    int x, y;
+    for (y = y0; y <= y1 ; y++) {
+        for(x = x0 ; x < x1 ; x++) {
+            vram[y * xsize + x] = c;
+        }
+    }
+    return;
 }
