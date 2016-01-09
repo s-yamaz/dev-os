a9ae944f92654d6ef6ace43f4012889627e04d4b
s.yamazaki <razgriz01blaze@gmail.com>
2016-01-09 14:52:38 +0900

    1文字表示する

--- src/c/bootpack.c
+++ src/c/bootpack.c
@@ -25,6 +25,8 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize,
         unsigned char c, int x0, int y0, int x1, int y1);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void init_screen(char *vram, int xsize, int ysize);
 
 struct BOOTINFO {
     char cyls, leds, vmode, reserve;
@@ -44,24 +46,13 @@ void SosMain(void) {
     ysize = (*binfo).scrny;
     vram = (*binfo).vram;
 
-    boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
-    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
-    boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
-    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
+    char font_A[16] = {
+        0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+        0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+    };
 
-    // スタートボタン
-    boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
-    boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
-    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
-    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
-    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
-    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
-
-    // 時刻表示領域
-    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
-    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
-    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 24, xsize - 4, ysize - 3);
-    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+    init_screen(binfo -> vram, binfo -> scrnx, binfo -> scrny);
+    putfont8(binfo -> vram, binfo -> scrnx, 10, 10, COL8_FFFFFF, font_A);
 
     for (;;) {
         io_hlt();
@@ -113,6 +104,47 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c,
         for(x = x0 ; x < x1 ; x++) {
             vram[y * xsize + x] = c;
         }
+    }
+    return;
+}
+
+void init_screen(char *vram, int xsize, int ysize) {
+
+    boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
+    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
+    boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
+    boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
+
+    // スタートボタン
+    boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
+    boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
+    boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
+    boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
+    boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
+
+    // 時刻表示領域
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 24, xsize - 4, ysize - 3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+    return;
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font) {
+    int i;
+    char *p, d;
+    for (i = 0 ; i < 16 ; i++) {
+        p = vram + (y + i) * xsize + x;
+        d = font[i];
+        if( (d & 0x80) != 0) { p[0] = c; }
+        if( (d & 0x40) != 0) { p[1] = c; }
+        if( (d & 0x20) != 0) { p[2] = c; }
+        if( (d & 0x10) != 0) { p[3] = c; }
+        if( (d & 0x08) != 0) { p[4] = c; }
+        if( (d & 0x04) != 0) { p[5] = c; }
+        if( (d & 0x02) != 0) { p[6] = c; }
+        if( (d & 0x01) != 0) { p[7] = c; }
     }
     return;
 }
